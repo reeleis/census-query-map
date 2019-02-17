840ff5344d38c3c736cea476ead5c74799354894
reeleis <lee.reis@berkeley.edu>
2019-02-17 11:24:41 -0800

    cleaned up some unnecessary comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 var tractdata = 0;
 
 const starturl='https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/Tracts_Blocks/MapServer/7/query?text=&geometry={x:-122.259233,y:37.849112,spatialReference:{wkid:4326}}&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&returnGeometry=true&geometryPrecision=7&outFields=STATE,COUNTY,TRACT,GEOID&outSR=4269&f=geojson';
-//     fetch(starturl)
-//       .then((resp) => resp.json()) // Transform the data into json
-//       .then(function(data) {
-//         console.log(data);
-//         tractdata = data
-//         })
+
 
 function createGISquery (extent, count='false') {
   return 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/Tracts_Blocks/MapServer/7/query?text=&geometry={%22xmin%22:'+ extent["_southWest"]["lng"]+',%22ymin%22:'+extent["_southWest"]["lat"]+',%22xmax%22:'+extent["_northEast"]["lng"]+',%22ymax%22:'+extent["_northEast"]["lat"]+',spatialReference:{wkid:4326}}'+'&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&returnGeometry=true&geometryPrecision=5&outFields=STATE,COUNTY,TRACT,GEOID&outSR=4269&f=geojson&returnCountOnly='+count;
@@ -15,20 +10,9 @@ function createGISquery (extent, count='false') {
 
 var myFeaturesMap = {};
 
-// var myGeoJsonLayerGroup = L.geoJson({
-//     onEachFeature: function (feature, layer) {
-//         myFeaturesMap[feature.properties.GEOID] = layer;
-//     }
-// }).addTo(map);
-
 function addNewFeatureToGeoJsonLayerGroup(newGeoJsonData) {
     overlay.addData(newGeoJsonData);
 }
-
-// function updateFeature(updatedGeoJsonData) {
-//     deleteFeature(updatedGeoJsonData); // Remove the previously created layer.
-//     addNewFeatureToGeoJsonLayerGroup(updatedGeoJsonData); // Replace it by the new data.
-// }
 
 function deleteFeature(keyToDelete) {
     var deletedFeature = myFeaturesMap[keyToDelete];
